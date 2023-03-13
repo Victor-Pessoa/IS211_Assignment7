38cdf4e6e2e683d4a269b6b4f393a67fc8e7167a
Victor-Pessoa <123887770+Victor-Pessoa@users.noreply.github.com>
2023-03-13 18:03:01 -0400

    Update pig.py

--- pig.py
+++ pig.py
@@ -1,51 +1,44 @@
-import argparse
 import random
 
 random.seed(0)
 
-
-def get_args():
-    parser = argparse.ArgumentParser(description='Play a game of Pig!')
-    parser.add_argument('--numPlayers', type=int, default=2, help='Number of players')
-    return parser.parse_args()
-
-
-def roll():
+def roll_die():
     return random.randint(1, 6)
 
+def take_turn(player):
+    print(f"\nPlayer {player + 1}'s turn:")
+    turn_score = 0
+    while True:
+        roll = roll_die()
+        if roll == 1:
+            print("You rolled a 1. Turn over.")
+            return 0
+        print(f"You rolled a {roll}.")
+        turn_score += roll
+        print(f"Your current score for this turn is {turn_score}.")
+        decision = input(f"Your total score is {players[player]}. Press (r) to roll or (h) to hold ")
+        while decision not in ['r', 'h']:
+            print("Invalid input. Please enter (r) to roll or (h) to hold.")
+            decision = input(f"Your total score is {players[player]}. Press (r) to roll or (h) to hold ")
+        if decision == "h":
+            players[player] += turn_score
+            return turn_score
 
-def play(playerScores):
-    currentPlayer = 0
-    currentScore = 0
+def play_game(num_players):
+    scores = [0] * num_players
+    player = 0
+    while max(scores) < 100:
+        print(f"\nCurrent scores: {scores}")
+        turn_score = take_turn(player)
+        scores[player] += turn_score
+        print(f"Player {player + 1}'s score is now {scores[player]}")
+        if scores[player] >= 100:
+            print(f"Player {player + 1} wins!")
+            return
+        player = (player + 1) % num_players
 
-    while max(playerScores) < 100:
-        print(f"\nPlayer {currentPlayer + 1}'s turn:")
-        print(f"Current score for this turn: {currentScore}")
-        print(f"Player scores: {playerScores}")
+if __name__ == "__main__":
+    num_players = int(input("How many players? "))
+    players = [0] * num_players
 
-        decision = input("Enter 'r' to roll or 'h' to hold: ")
-        if decision == 'r':
-            rollResult = roll()
-            print(f"Player {currentPlayer + 1} rolled a {rollResult}")
-
-            if rollResult == 1:
-                print("Rolled a 1. Turn over, no points earned.")
-                currentScore = 0
-                currentPlayer = (currentPlayer + 1) % numPlayers
-            else:
-                currentScore += rollResult
-
-        elif decision == 'h':
-            playerScores[currentPlayer] += currentScore
-            print(f"Player {currentPlayer + 1} scored {currentScore} points this turn")
-            currentScore = 0
-            currentPlayer = (currentPlayer + 1) % numPlayers
-
-    print(f"\nPlayer {playerScores.index(max(playerScores)) + 1} wins with {max(playerScores)} points!")
-
-
-if __name__ == '__main__':
-    args = get_args()
-    numPlayers = args.numPlayers
-    playerScores = [0] * numPlayers
-    play(playerScores)
+    play_game(num_players)
